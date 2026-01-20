6a97f29d64a6ba38e4787a76516ca74514bff9c0
Guejuks <Guejuks@gmail.com>
2026-01-20 10:05:50 +0700

    Initial commit

---
+++ app/Models/Category.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    /**
+     * fillable
+     */
+    protected $fillable = ['name', 'slug', 'image'];
+
+    /**
+     * accessor image category
+     */
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => asset('/storage/categories/' . $image),
+        );
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /**
+     * fillable
+     */
+    protected $fillable = ['name', 'slug', 'category_id', 'image', 'price', 'quantity'];
+
+    /**
+     * accessor image products
+     */
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => asset('/storage/products/' . $image),
+        );
+    }
+
+    /**
+     * relation category
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * relation transaction detail
+     */
+    public function transaction_details()
+    {
+        return $this->hasMany(TransactionDetail::class);
+    }
+}
